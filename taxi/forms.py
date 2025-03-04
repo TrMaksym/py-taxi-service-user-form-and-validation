112010a73b641ef25b37db82f6191283628f5083
unknown <maximuschampion2002@gmail.com>
2025-03-04 22:11:02 +0200

    taxi-service-user-form-and-validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,34 +1,63 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 from taxi.models import Driver, Car
 
 
-class DriverCreationForm(UserCreationForm):
-
-    class Meta(UserCreationForm.Meta):
-        model = Driver
-        fields = UserCreationForm.Meta.fields + ('license_number',)
-
-
-class DriverLicenseUpdateForm(forms.ModelForm):
-
-    class Meta:
-        model = Driver
-        fields = ["license_number"]
+class LicenseNumberValidatorMixin:
+    license_number = forms.CharField(
+        required=True,
+        validators=[MinLengthValidator(8), MaxLengthValidator(8)]
+    )
 
     def clean_license_number(self):
-        license_number = self.cleaned_data.get("license_number")
+
+        license_number = self.cleaned_data["license_number"]
+
         if len(license_number) != 8:
-            raise forms.ValidationError("Invalid license number")
-        if not license_number[:3].isupper():
-            raise forms.ValidationError("Invalid license number")
-        if not license_number[:5].isdigit():
-            raise forms.ValidationError("Invalid license number")
+            raise ValidationError(
+                "License number must be exactly 8 characters long."
+            )
+
+        if (
+                not license_number[:3].isupper()
+                or not license_number[:3].isalpha()
+        ):
+            raise ValidationError(
+                "First 3 characters of license number "
+                "must be uppercase letters"
+            )
+
+        if not license_number[3:].isdigit():
+            raise ValidationError(
+                "Last 5 characters of license number must be digits"
+            )
 
         return license_number
 
-class DriverDeleteForm(forms.ModelForm):
-    model = Driver
-    fields = ["license_number"]
+
+class DriverCreationForm(UserCreationForm, LicenseNumberValidatorMixin):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("license_number",)
+
+
+class DriverLicenseUpdateForm(forms.ModelForm, LicenseNumberValidatorMixin):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+
+class CarCreationForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Car
+        fields = ("model", "manufacturer", "drivers")
--- taxi/urls.py
+++ taxi/urls.py
@@ -10,32 +10,60 @@ from .views import (
     DriverListView,
     DriverDetailView,
     DriverCreateView,
+    DriverLicenseUpdateView,
     DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView, DriverUpdateView, remove_driver_from_car, assign_driver_to_car,
+    ManufacturerDeleteView,
+    assign_me_car,
+    delete_me_car,
 )
-
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
-    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
-    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
-    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list",
+    ),
+    path(
+        "manufacturers/create/",
+        ManufacturerCreateView.as_view(),
+        name="manufacturer-create",
+    ),
+    path(
+        "manufacturers/<int:pk>/update/",
+        ManufacturerUpdateView.as_view(),
+        name="manufacturer-update",
+    ),
+    path(
+        "manufacturers/<int:pk>/delete/",
+        ManufacturerDeleteView.as_view(),
+        name="manufacturer-delete",
+    ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path(
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/update-license/", DriverUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
-    path('cars/<int:pk>/assign-driver/', assign_driver_to_car, name='assign-driver-to-car'),
-    path('cars/<int:pk>/remove-driver/', remove_driver_from_car, name='remove-driver-from-car'),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/driver-update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path("assing-me-car/<int:pk>/", assign_me_car, name="assign-me-car"),
+    path("delete-me-car/<int:pk>/", delete_me_car, name="delete-me-car"),
 ]
 
 app_name = "taxi"
